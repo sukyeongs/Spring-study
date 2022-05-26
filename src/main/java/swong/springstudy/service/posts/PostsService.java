70ce4c32ddef49d21aee0dc2b56b07de39134ab5
sukyeong <ssk0967@naver.com>
2022-05-26 14:51:50 +0900

    posts 관련 테스트, 메인코드 작성
    조회, 업데이트 기능 구현 (BaseTimeEntity 사용-> 생성, 수정 시간 상속)

---
+++ src/main/java/swong/springstudy/service/posts/PostsService.java
@@ -0,0 +1,39 @@
+package swong.springstudy.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import swong.springstudy.domain.posts.Posts;
+import swong.springstudy.domain.posts.PostsRepository;
+import swong.springstudy.web.dto.PostsResponseDto;
+import swong.springstudy.web.dto.PostsSaveRequestDto;
+import swong.springstudy.web.dto.PostsUpdateRequestDto;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        return new PostsResponseDto(entity);
+    }
+}
