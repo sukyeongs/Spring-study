7d222a85e104b826104ba8a25f8de325c929149c
sukyeong <ssk0967@naver.com>
2022-06-04 14:53:23 +0900

    feat: add delete logic

--- src/main/java/swong/springstudy/service/posts/PostsService.java
+++ src/main/java/swong/springstudy/service/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import swong.springstudy.domain.posts.Posts;
 import swong.springstudy.domain.posts.PostsRepository;
+import swong.springstudy.web.dto.PostsListResponseDto;
 import swong.springstudy.web.dto.PostsResponseDto;
 import swong.springstudy.web.dto.PostsSaveRequestDto;
 import swong.springstudy.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -35,5 +39,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
